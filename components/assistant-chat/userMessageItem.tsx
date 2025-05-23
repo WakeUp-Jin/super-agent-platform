a0ea91b23d0b7a23485674919a9ec1697212e280
WakeUp-Jin <2385472291@qq.com>
2025-05-23 12:26:28 +0800

    feat:用户消息组件搞定

--- components/assistant-chat/userMessageItem.tsx
+++ components/assistant-chat/userMessageItem.tsx
@@ -1,16 +1,28 @@
-import { Bold } from 'lucide-react';
+import { Bold, Copy, Edit, Pencil } from 'lucide-react';
 import { Toggle } from '../ui/toggle';
+import { Button } from '../ui/button';
 
 export function UserMessageItem() {
   return (
-    <div className="flex w-full flex-col justify-end">
-      <div className="max-w-sm rounded-lg bg-gray-50 p-4 break-words shadow">
-        这里是一段很长很长很长很长很长很长的文字，如果文字超出了最大宽度就会自动换行，没有最大高度限制，能一直往下撑开……
-      </div>
-      <div className="max-w-sm">
-        <Toggle aria-label="Toggle italic">
-          <Bold className="h-4 w-4" />
-        </Toggle>
+    <div className="flex w-full justify-end">
+      <div className="flex flex-col">
+        <div className="max-w-sm rounded-lg bg-gray-50 p-4 break-words shadow">
+          这里是一段很长很长很长很长很长很长的文字，如果文字超出了最大宽度就会自动换行，没有最大高度限制，能一直往下撑开……
+        </div>
+        <div className="max-w-sm">
+          <Toggle
+            aria-label="Toggle italic"
+            className="cursor-pointer border-none hover:bg-gray-200"
+          >
+            <Pencil className="h-4 w-4" />
+          </Toggle>
+          <Toggle
+            aria-label="Toggle italic"
+            className="cursor-pointer border-none hover:bg-gray-200"
+          >
+            <Copy className="h-4 w-4" />
+          </Toggle>
+        </div>
       </div>
     </div>
   );
